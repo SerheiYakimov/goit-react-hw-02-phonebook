ce344b5d320a65cdd419814a094b420a6a0e9ed5
Serhei <ysergey2901@gmail.com>
2021-10-17 03:01:02 +0300

    add input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
+import { Component } from "react";
 import "./App.css";
+import { v4 as uuid } from "uuid";
+// import Form from './components/Form/Form';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  idName = uuid();
+
+  hendleChange = (e) => {
+    console.log(e.currentTarget.value);
+
+    this.setState({
+      name: e.currentTarget.value,
+    });
+  };
+
+  hendleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Phonebook</h2>
+        <form onSubmit={this.hendleSubmit}>
+          <label htmlFor={this.idName}>
+            Name
+            <input
+              type="text"
+              name="name"
+              id={this.idName}
+              value={this.state.name}
+              onChange={this.hendleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <div>
+          <h2>Contacts</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,23 @@
+// import { Component } from "react";
+
+// class Form extends Component {
+
+//     render() {
+//         return (
+//             <form onSubmit={this.hendleSubmit}>
+//         <label>
+//           <input
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             onChange={this.hendleChange}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//             required
+//           />
+//         </label>
+//         <button type="submit">Add contact</button>
+//       </form>
+//         )
+//     }
+// }
